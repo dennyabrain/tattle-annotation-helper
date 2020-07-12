c6a0137d13e525057cefbec7d39beeb743806825
denny <denny.george90@gmail.com>
2020-07-13 01:28:16 +0530

    (feat) added core ui components

---
+++ src/config/options.js
@@ -0,0 +1,20 @@
+export const footerItems = {
+  primary: [
+    {
+      id: 1,
+      label: "Privacy Policy",
+      target: "/privacy-policy",
+      type: "internal",
+    },
+    {
+      id: 2,
+      label: "FAQ",
+      target: "/faq",
+      type: "internal",
+    },
+  ],
+}
+
+export const primaryNav = {
+  options: [],
+}
